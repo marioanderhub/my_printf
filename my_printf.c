334984b78571c46f9c08f267f419651f8d48a6d1
mario anderhub <anderhu_m@qwasar.io>
2023-11-18 22:48:34 +0000

    project setup

---
+++ my_printf.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+
+/*
+*
+* Simplified implementation of printf.
+*
+* @param char *restrict format
+* @param ...
+*
+* @return: The number of printed characters exluding the NULL byte; if an error occurs a negative value is returned
+*
+*/
+int my_printf(char *restrict format, ...) {
+    int chars_printed = 0;
+
+    return chars_printed;
+}
+
+int main(void) {
+
+    return 0;
+}
