9351cc69129def6ac16087588357da4b1105104c
mario anderhub <anderhu_m@qwasar.io>
2023-11-20 06:53:51 +0000

    notes and ideas

--- my_printf.c
+++ my_printf.c
@@ -1,19 +1,52 @@
 #include <stdarg.h>
 
 /*
+Notes:
+
+normal characters (unequal %) are copied to stdout
+a conversion specification is introduced by % followed by a conversion specifier
+
+parse format
+    calculate output string size
+        look for % and specifier
+        convert type to max possible chars
+build the entire string
+    iterate format again
+    copy chars
+    if conversion specifier found -> convert to str
+    copy to output str
+write
+    write output str in one go to stdout
+*/
+
+/*
 *
-* Simplified implementation of printf.
+* 
 *
 * @param char *restrict format
 * @param ...
 *
-* @return: The number of printed characters exluding the NULL byte; if an error occurs a negative value is returned
+* @return: The number of printed characters exluding the NULL byte; if an error occurs a negative number is returned
 *
 */
 int my_printf(char *restrict format, ...) {
-    int chars_printed = 0;
+    va_list args;
+    va_start(args, format);
 
-    return chars_printed;
+
+    int index = 0;
+    int max_size = 0;
+
+    while (format[index] != '\0') {
+        if (format[index] == '%') {
+
+        }
+        max_size++;
+        index++;
+    }
+
+
+    return 0;
 }
 
 int main(void) {
